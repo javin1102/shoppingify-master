720e4d54637539d097ed11955b819a623f877035
javin1102 <javinrio112@gmail.com>
2022-02-18 16:26:33 +0700

    set up cors api route

--- server/routes/api.php
+++ server/routes/api.php
@@ -15,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/auth/register', [UserController::class, 'register']);
-Route::post('/auth/login', [UserController::class, 'login']);
+Route::middleware(['cors'])->group(function () {
+    Route::post('/auth/register', [UserController::class, 'register']);
+    Route::post('/auth/login', [UserController::class, 'login']);
+});
